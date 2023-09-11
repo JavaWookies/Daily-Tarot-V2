1451ca8c369f4781054a409debe5b591ffdb9f95
Aaron Clark <amclarkwustl@gmail.com>
2023-09-11 13:30:21 -0700

    setup manifest to initialize ampliy app

---
+++ app/src/main/java/com/amcwustl/dailytarot/DailyTarotAmplifyApplication.java
@@ -0,0 +1,30 @@
+package com.amcwustl.dailytarot;
+
+import android.app.Application;
+import android.util.Log;
+
+
+import com.amplifyframework.AmplifyException;
+import com.amplifyframework.api.aws.AWSApiPlugin;
+import com.amplifyframework.auth.cognito.AWSCognitoAuthPlugin;
+import com.amplifyframework.core.Amplify;
+import com.amplifyframework.auth.cognito.AWSCognitoAuthPlugin;
+import com.amplifyframework.storage.s3.AWSS3StoragePlugin;
+
+
+public class DailyTarotAmplifyApplication extends Application {
+    public static final String TAG = "DailyTarotApplication";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        try {
+            Amplify.addPlugin(new AWSApiPlugin());
+            Amplify.configure(getApplicationContext());
+            Log.i("MyAmplifyApp", "Initialized Amplify");
+        } catch (AmplifyException error) {
+            Log.e("MyAmplifyApp", "Could not initialize Amplify", error);
+        }
+    }
+}
