21b765dfaee75afb487c37333c24a34177df60a8
AlexSaeChao <chaoalex93@gmail.com>
2023-09-11 15:25:52 -0700

    used cognito to take in the fields from signup, verify, and login page. Can now add users to the app via cognito.

--- app/src/main/java/com/amcwustl/dailytarot/MainActivity.java
+++ app/src/main/java/com/amcwustl/dailytarot/MainActivity.java
@@ -4,10 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+
 import android.widget.ImageView;
-
 import com.amcwustl.dailytarot.activities.SignUpActivity;
-
+import com.amplifyframework.core.Amplify;
 
 public class MainActivity extends AppCompatActivity {
   private final String TAG = "MainActivity";
@@ -18,7 +19,14 @@ public class MainActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
+    Amplify.Auth.fetchAuthSession(
+            result -> Log.i("AmplifyQuickstart", result.toString()),
+            error -> Log.e("AmplifyQuickstart", error.toString())
+    );
+
       moveToSignUp = findViewById(R.id.MainActivitySignUpImageView);
+
+
 
       setupMovetoSignup();
   }
--- app/src/main/java/com/amcwustl/dailytarot/activities/SignUpActivity.java
+++ app/src/main/java/com/amcwustl/dailytarot/activities/SignUpActivity.java
@@ -4,10 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 
 import com.amcwustl.dailytarot.R;
+import com.amplifyframework.auth.AuthUserAttributeKey;
+import com.amplifyframework.auth.options.AuthSignUpOptions;
+import com.amplifyframework.core.Amplify;
 
 public class SignUpActivity extends AppCompatActivity {
   public static final String TAG = "SignupActivity";
@@ -29,12 +33,26 @@ public class SignUpActivity extends AppCompatActivity {
 
   }
 
-// TODO: Setup intent and onclick confirm sign-up with AWS
-void setupSubmitButton() {
-  submitButton.setOnClickListener(v -> {
-    Intent goToLoginIntent = new Intent(SignUpActivity.this, VerifyActivity.class);
-    startActivity(goToLoginIntent);
-  });
-}
+  void setupSubmitButton() {
+    submitButton.setOnClickListener(v -> {
+      Amplify.Auth.signUp(
+              emailEditText.getText().toString(),
+              passwordEditText.getText().toString(),
+              AuthSignUpOptions.builder()
+                      .userAttribute(AuthUserAttributeKey.email(), emailEditText.getText().toString())
+                      .build(),
+              successResponse -> {
+                Log.i(TAG, "Signup succeeded: " + successResponse.toString());
 
+                Intent goToVerifyActivity = new Intent(SignUpActivity.this, VerifyActivity.class);
+                goToVerifyActivity.putExtra("email", emailEditText.getText().toString());
+
+                startActivity(goToVerifyActivity);
+              },
+              failureResponse -> {
+                Log.i(TAG, "Signup failed: " + failureResponse.toString());
+              }
+      );
+    });
+  }
 }
