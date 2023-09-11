85e6c50b73503a1a19a39d68c1085d5f3e5ef425
Aaron Clark <amclarkwustl@gmail.com>
2023-09-11 11:37:15 -0700

    setup dynamoDB

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "dailytarot": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
