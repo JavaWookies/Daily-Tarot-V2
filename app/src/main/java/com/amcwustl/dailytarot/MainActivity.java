6851094090d0dc13a3c2871f4377caa1945b924e
Aaron Clark <amclarkwustl@gmail.com>
2023-11-25 15:32:28 -0800

    updated home page UI with new fonts

--- app/src/main/java/com/amcwustl/dailytarot/MainActivity.java
+++ app/src/main/java/com/amcwustl/dailytarot/MainActivity.java
@@ -1,13 +1,19 @@
 package com.amcwustl.dailytarot;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
+
+import androidx.preference.PreferenceManager;
 
 import com.amcwustl.dailytarot.activities.BaseActivity;
 import com.amcwustl.dailytarot.activities.DailyCardActivity;
 import com.amcwustl.dailytarot.activities.ReadingActivity;
+import com.amcwustl.dailytarot.activities.UserSettingsActivity;
 import com.amcwustl.dailytarot.activities.ViewAllCardsActivity;
 import com.amcwustl.dailytarot.data.CardDbHelper;
 
@@ -15,10 +21,17 @@ import com.amcwustl.dailytarot.data.CardDbHelper;
 
 @SuppressWarnings("resource")
 public class MainActivity extends BaseActivity {
+  private static final String TAG = "Home Activity";
 
   private FrameLayout cardDailyReading;
   private FrameLayout cardOfTheDay;
   private FrameLayout viewAllCards;
+  private ImageView cardOne;
+  private ImageView cardTwo;
+  private ImageView cardThree;
+  private ImageView cardFour;
+
+  SharedPreferences preferences;
 
 
   @Override
@@ -26,13 +39,22 @@ public class MainActivity extends BaseActivity {
     setContentView(R.layout.activity_main);
     super.onCreate(savedInstanceState);
 
+    preferences = PreferenceManager.getDefaultSharedPreferences(this);
     myNavView = findViewById(R.id.MainActivityNavigationView);
-
     cardDailyReading = findViewById(R.id.card_daily_reading);
     cardOfTheDay = findViewById(R.id.card_of_the_day);
     viewAllCards = findViewById(R.id.view_all_cards);
 
+    cardOne = findViewById(R.id.imageView_card_1);
+    cardTwo = findViewById(R.id.imageView_card_2);
+    cardThree = findViewById(R.id.imageView_card_3);
+    cardFour = findViewById(R.id.imageView_card_4);
+
+
+
+
     setupCardNavigation();
+    setupCardTypes();
 
     CardDbHelper dbHelper = new CardDbHelper(this);
 
@@ -58,6 +80,22 @@ public class MainActivity extends BaseActivity {
       startActivity(viewAllCardsIntent);
     });
 
+
+  }
+
+  private void setupCardTypes() {
+    String cardType = preferences.getString(UserSettingsActivity.CARD_TYPE_TAG, "");
+    String resourceName = "cover" + cardType;
+
+    @SuppressLint("DiscouragedApi") int resourceId = getResources().getIdentifier(resourceName, "drawable", getPackageName());
+    if (resourceId != 0) {
+      cardOne.setImageResource(resourceId);
+      cardTwo.setImageResource(resourceId);
+      cardThree.setImageResource(resourceId);
+      cardFour.setImageResource(resourceId);
+    } else {
+      Log.e(TAG, "Resource not found for card type: " + cardType);
+    }
 
   }
 
