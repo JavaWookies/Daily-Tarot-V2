ddb01342757d834df9e9cee0850e513bd7dd60fb
Aaron Clark <amclarkwustl@gmail.com>
2023-11-27 16:45:40 -0800

    finished out custom spread implementation

--- app/src/main/java/com/amcwustl/dailytarot/activities/CustomSpreadActivity.java
+++ app/src/main/java/com/amcwustl/dailytarot/activities/CustomSpreadActivity.java
@@ -10,11 +10,13 @@ import android.content.ClipDescription;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
-import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.DragEvent;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -26,15 +28,21 @@ import androidx.preference.PreferenceManager;
 import com.amcwustl.dailytarot.R;
 import com.amcwustl.dailytarot.data.CardDbHelper;
 import com.amcwustl.dailytarot.models.Card;
+import com.google.android.gms.ads.rewarded.RewardedAd;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Objects;
 import java.util.Random;
 
 public class CustomSpreadActivity extends BaseActivity {
     private static final String TAG = "Custom Spread Activity";
+    private static final String CUSTOM_COINS = "custom_coins";
+    private static final String LAST_COIN_RESET_DATE = "last_coin_reset_date";
     private CardDbHelper dbHelper;
     private ImageView deckImageView;
     private FrameLayout droppableArea;
@@ -43,8 +51,13 @@ public class CustomSpreadActivity extends BaseActivity {
     private String cardType;
     private HashMap<Integer, Long> drawnCards;
     private int drawnCardCount = 0;
-    private List<ImageView> droppedImages;
+    private List<View> droppedImages;
     private LinearLayout lockLayout;
+    private LinearLayout instructionsLayout;
+    private LinearLayout resetLayout;
+
+    private int userCustomCoinCount = 0;
+    private RewardedAd rewardedAd;
 
 
 
@@ -57,9 +70,13 @@ public class CustomSpreadActivity extends BaseActivity {
         cardType = preferences.getString(UserSettingsActivity.CARD_TYPE_TAG, "");
 
         lockLayout = findViewById(R.id.lockLayout);
+        instructionsLayout = findViewById(R.id.instructionsLayout);
+        resetLayout = findViewById(R.id.clearLayout);
 
         deckImageView = findViewById(R.id.deckImageView);
         droppableArea = findViewById(R.id.droppableArea);
+
+
 
         dbHelper = new CardDbHelper(this);
 
@@ -69,7 +86,9 @@ public class CustomSpreadActivity extends BaseActivity {
         setupCardType();
         setupDeckTouchListener();
         setupDroppableArea();
-        setupLockLayoutButton();
+        setupInstructionsLayoutButton();
+        checkCustomReadingForToday();
+        loadCustomCoins();
     }
 
     private void setupCardType(){
@@ -107,8 +126,8 @@ public class CustomSpreadActivity extends BaseActivity {
 
     private ImageView createNewCardImageView() {
         ImageView imageView = new ImageView(this);
-        int widthInPx = convertDpToPx(100); // Convert 100dp to pixels
-        int heightInPx = convertDpToPx(145); // Convert 145dp to pixels
+        int widthInPx = convertDpToPx(100);
+        int heightInPx = convertDpToPx(171);
 
         imageView.setLayoutParams(new FrameLayout.LayoutParams(widthInPx, heightInPx));
         if(Objects.equals(cardType, "")){
@@ -117,6 +136,7 @@ public class CustomSpreadActivity extends BaseActivity {
             imageView.setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.cover_light, null));
         }
         imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+        imageView.setTag("newCard_" + drawnCardCount);
 
         return imageView;
     }
@@ -135,23 +155,64 @@ public class CustomSpreadActivity extends BaseActivity {
                     return true;
                 case DragEvent.ACTION_DROP:
                     draggedView = (View) dragEvent.getLocalState();
-                    draggedView.setX(dragEvent.getX() - draggedView.getWidth() / 2f);
-                    draggedView.setY(dragEvent.getY() - draggedView.getHeight() / 2f);
 
-                    long cardId = drawRandomCard();
-                    drawnCards.put(drawnCardCount, cardId);
-                    drawnCardCount ++;
-                    Log.d(TAG, "Drawn card count is: " + drawnCardCount);
-                    droppedImages.add((ImageView) draggedView);
-                    if (drawnCardCount < 9){
-//                        setupDeckTouchListener();
+                    int cardWidth = draggedView.getWidth();
+                    int cardHeight = draggedView.getHeight();
+
+                    float dropX = dragEvent.getX() - (cardWidth / 2f);
+                    float dropY = dragEvent.getY() - (cardHeight / 2f);
+
+                    int leftBound = droppableArea.getLeft();
+                    int topBound = droppableArea.getTop();
+                    int rightBound = droppableArea.getRight();
+                    int bottomBound = droppableArea.getBottom();
+
+                    if (dropX >= leftBound && dropY >= topBound &&
+                            dropX + cardWidth <= rightBound && dropY + cardHeight <= bottomBound) {
+                        draggedView.setX(dropX);
+                        draggedView.setY(dropY);
+                        handleCardDropLogic(draggedView);
+                        setupCardTouchListener(draggedView);
                     } else {
-                        lockLayout();
+                        ((ViewGroup) draggedView.getParent()).removeView(draggedView);
+                        return false;
                     }
                     return true;
             }
             return false;
         });
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private void setupCardTouchListener(View cardView) {
+        cardView.setOnTouchListener((v, event) -> {
+            if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                ClipData data = ClipData.newPlainText("", "");
+                View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(v);
+                v.startDragAndDrop(data, shadowBuilder, v, 0);
+                return true;
+            }
+            return false;
+        });
+    }
+
+    private void handleCardDropLogic(View draggedView){
+        Object tag = draggedView.getTag();
+        if (tag != null && tag.toString().startsWith("newCard_")){
+            long cardId = drawRandomCard();
+            drawnCards.put(drawnCardCount, cardId);
+            drawnCardCount++;
+            if (drawnCardCount == 1){
+                setupLockLayoutButton();
+                setupResetButton();
+            }
+            droppedImages.add((ImageView) draggedView);
+            draggedView.setTag(null);
+
+            if (drawnCardCount == 9){
+                deckImageView.setOnLongClickListener(null);
+            }
+        }
     }
 
     private long drawRandomCard(){
@@ -173,7 +234,7 @@ public class CustomSpreadActivity extends BaseActivity {
 
             int goldColor = Color.parseColor("#FFD700");
 
-            lockIcon.setColorFilter(goldColor, PorterDuff.Mode.SRC_IN);
+            lockIcon.setImageResource(R.drawable.baseline_lock_24);
 
             lockText.setTextColor(goldColor);
 
@@ -182,18 +243,20 @@ public class CustomSpreadActivity extends BaseActivity {
         });
     }
 
+    @SuppressWarnings("ConstantConditions")
+    @SuppressLint("ClickableViewAccessibility")
     private void lockLayout(){
         deckImageView.setOnLongClickListener(null);
-        Log.d(TAG, "Method is being called");
 
-        for (int i = 0; i < drawnCardCount;i++){
-            int finalI = i;
-            ImageView card = droppedImages.get(i);
-            card.setOnClickListener(view -> {
-                flipCard(card, drawnCards.get(finalI));
-            });
+
+        for (View cardView : droppedImages){
+
+            ImageView card = (ImageView) cardView;
+
+            card.setOnTouchListener(null);
+
+            card.setOnClickListener(view -> flipCard(card, drawnCards.get(droppedImages.indexOf(cardView))));
         }
-
     }
 
     private void flipCard(ImageView card, long cardId) {
@@ -230,6 +293,7 @@ public class CustomSpreadActivity extends BaseActivity {
             });
 
             firstHalfFlip.start();
+            card.setOnClickListener(null);
             card.setOnLongClickListener(view -> {
                 navigateToCardDetail(drawnCard.getId());
                 return true;
@@ -243,6 +307,82 @@ public class CustomSpreadActivity extends BaseActivity {
         Intent intent = new Intent(CustomSpreadActivity.this, CardDetailActivity.class);
         intent.putExtra("card_id", cardId);
         startActivity(intent);
+    }
+
+    private void setupInstructionsLayoutButton(){
+        instructionsLayout.setOnClickListener(view -> showInstructionsModal());
+    }
+
+    private void showInstructionsModal() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        View view = getLayoutInflater().inflate(R.layout.interpretation_modal, null);
+        TextView tvInterpretationContent = view.findViewById(R.id.tv_interpretation_content);
+        tvInterpretationContent.setText(getString(R.string.custom_instructions_text));
+        Button btnCloseModal = view.findViewById(R.id.btn_close_modal);
+        btnCloseModal.setOnClickListener(v -> instructionsDialog.dismiss());
+
+        builder.setView(view);
+        instructionsDialog = builder.create();
+        instructionsDialog.show();
+
+    }
+
+    private void setupResetButton() {
+        resetLayout.setOnClickListener(view -> resetLayout());
+    }
+
+    private void resetLayout() {
+        if (userCustomCoinCount >= 10) {
+            userCustomCoinCount -= 10;
+            saveCustomCoins();
+            performReset();
+        } else {
+            // Show rewarded ad here to give the user 20 more coins
+            showRewardedAd();
+        }
+    }
+
+    private void performReset() {
+        drawnCards.clear();
+        drawnCardCount = 0;
+        for (View cardView : droppedImages) {
+            droppableArea.removeView(cardView);
+        }
+        droppedImages.clear();
+
+        ImageView lockIcon = findViewById(R.id.lockIcon);
+        TextView lockText = findViewById(R.id.lockText);
+
+        int whiteColor = Color.parseColor("#FFFFFF");
+
+        lockIcon.setImageResource(R.drawable.baseline_lock_open_24);
+
+        lockText.setTextColor(whiteColor);
+
+        setupDeckTouchListener();
+    }
+
+    private void checkCustomReadingForToday() {
+        String lastResetDate = preferences.getString(LAST_COIN_RESET_DATE, "");
+        String todayDate = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(new Date());
+
+        if (!todayDate.equals(lastResetDate)) {
+            userCustomCoinCount = 20;
+            saveCustomCoins();
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.putString(LAST_COIN_RESET_DATE, todayDate);
+            editor.apply();
+        }
+    }
+
+    private void loadCustomCoins() {
+        userCustomCoinCount = preferences.getInt(CUSTOM_COINS, 20);
+    }
+
+    private void saveCustomCoins() {
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putInt(CUSTOM_COINS, userCustomCoinCount);
+        editor.apply();
     }
 
 }
